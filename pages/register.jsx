eb89ffe7cf3d5a3b36fae892d3e89818f6095f75
Abdul Hadi <hadi22905@gmail.com>
2023-08-16 16:14:29 +0500

    commit

---
+++ pages/register.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const register = () => {
+  return (
+    <>
+      <div className="register-main-div">
+      <iframe className="registerForm" src="https://docs.google.com/forms/d/e/1FAIpQLScDHgMpuqVTC-gE1iKPbPO-9wruqPpTP0pUm71M36Ino9Kt6w/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>        {/* <div className="form-main">
+          <div className="form-title">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="form-div-2">
+            <h2>Register as</h2>
+          </div>
+
+          <form
+            action="https://docs.google.com/forms/d/e/1FAIpQLSea9h3zsNVq5zKyaGSs84OWAf5HhAwS0R8YMdEsP1ci_gqgKw/formResponse
+"
+ method="post"
+// target="hiddenConfirm"
+// onSubmit="submitted=true"
+          >
+            <div className="designtion-radio">
+              <input type="radio" id="Ambassador" name="entry.2143642858" value="Ambassador"/>
+              <label htmlFor="Ambassador">
+                Ambassador (Choose this option if you have ambassador code)
+              </label>
+              <input type="radio" name="entry.2143642858" value="private" id="private"/>
+              <label htmlFor="private">Private</label>
+            </div>
+            <div className="full-name text-fields">
+              <label htmlFor="">Full Name</label>
+              <input type="text" />
+            </div>
+            <div className="gender-radio">
+              <input type="radio" />
+              <label htmlFor="">Male</label>
+              <input type="radio" />
+              <label htmlFor="">Female</label>
+            </div>
+
+            <div className="text-fields">
+              <label htmlFor="">CNIC</label>
+              <input type="text" />
+            </div>
+            <div className="text-fields">
+              <label htmlFor="">Contact</label>
+              <input type="text" />
+            </div>
+            <div className="text-fields">
+              <label htmlFor="">Email</label>
+              <input type="text" />
+            </div>
+            <div className="text-fields">
+              <label htmlFor="">Username</label>
+              <input type="text" />
+            </div>
+            <div className="text-fields">
+              <label htmlFor="">Password</label>
+              <input type="text" />
+            </div>
+            <div className="text-fields">
+              <label htmlFor="">Confirm Password</label>
+              <input type="text" />
+            </div>
+
+            <button type="submit">Signup</button>
+          </form>
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default register;
